2beaf598ab6c6f3235bd4354734133ce124c4c53
Rassul Serik <bamblbeeprime@gmail.com>
2025-07-22 22:39:09 +0500

    Исправлена ошибкаподключения

--- parser.py
+++ parser.py
@@ -13,12 +13,13 @@ load_dotenv()
 # Подключение к базе
 try:
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    sslmode="require"
+)
     cursor = conn.cursor()
 except Exception as e:
     print("❌ Ошибка подключения к базе:", e)
