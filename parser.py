e882d49c88640962a1450add11fbdb80a6962473
Rassul Serik <bamblbeeprime@gmail.com>
2025-07-22 21:22:49 +0500

    Добавлена задержка перед завершением

--- parser.py
+++ parser.py
@@ -1,4 +1,8 @@
 import datetime
+import time
 
 print("Привет! Парсер запустился.")
 print("Текущее время:", datetime.datetime.now())
+
+# Задержка перед завершением (например, 5 минут)
+time.sleep(300)
